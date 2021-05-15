1c5591780eaf4b6e641ffa526a57726f06742fcb
mevluthan <58922055+mevluthan@users.noreply.github.com>
2021-05-15 03:19:35 +0300

    GET method

---
+++ README.md
@@ -0,0 +1,1 @@
+# SimpleServletApplicationV2
---
+++ src/main/java/org/example/servlet/Servlet.java
@@ -0,0 +1,30 @@
+package org.example.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet
+ */
+@WebServlet(description = "A Simple Servlet", urlPatterns = { "/ServletPath" })
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("Hello from GET method.");
+		PrintWriter pw= response.getWriter();
+		pw.println("<h3>Hello in html</h3>");
+	}
+
+}
